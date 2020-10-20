e20a88a4e1cc83ab6be025d063b191267185e292
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-10-20 23:19:19 +0900

    excuse makemigrations, migrate

--- api/models.py
+++ api/models.py
@@ -35,7 +35,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
-    is_staff = models.CharField(default=False)
+    is_staff = models.CharField(max_length=255, default=False)
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
