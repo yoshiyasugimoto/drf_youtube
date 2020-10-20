ece5bf46b792ab2c396c31a8511fabf9ef3f924a
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-10-20 23:59:54 +0900

    Create views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from .serializers import VideoSerializers, UserSerializer
+from rest_framework.permissions import AllowAny
+from .models import Video
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializers
+    
